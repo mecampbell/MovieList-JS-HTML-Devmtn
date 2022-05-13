94a9984f28bb30c37539095f9514a2187aabb726
Michael Campbell <michaelmichael323@hotmail.com>
2022-05-12 19:39:21 -0600

    finalized

---
+++ index.js
@@ -0,0 +1,46 @@
+const message = document.querySelector('#message')
+document.querySelector('form').addEventListener('submit', addMovie);
+
+function addMovie (event) {
+    event.preventDefault();
+    let inputField = document.querySelector('input');
+    
+    const movie = document.createElement('li');
+    const movieTitle = document.createElement('span');
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener('click', crossOffMovie);
+    movie.appendChild(movieTitle);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener('click', deleteMovie);
+    movie.appendChild(deleteBtn);
+    
+    const list = document.querySelector('ul');
+    list.appendChild(movie);
+      
+    inputField.value = '';
+}
+
+function deleteMovie (event) {
+    event.target.parentNode.remove();
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`
+}
+
+function crossOffMovie (event) {
+    event.target.classList.toggle('checked')
+
+    if (event.target.classList.contains('checked') === true) {
+        message.textContent = `${event.target.textContent} watched!`
+    } else {
+        message.textContent = `${event.target.textContent} added back!`
+    }
+    revealMessage();
+}
+
+function revealMessage () {
+    message.classList.remove('hide');
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000);
+};
